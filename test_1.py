fixed top 5 accuracy calculation and flag for calculating overall accuracy

test_1.py
@@ -84,7 +84,8 @@ pred_stack=[]
 target_stack=[]
 correct_predicted_labels = torch.zeros(1000,dtype=torch.float64)
 total_labels = torch.zeros(1000,dtype=torch.float64)
-calculate_per_class_acc = False
+calculate_per_class_acc = True
+calculate_overall_acc = False
 
 def main():
     args = parser.parse_args()
@@ -348,8 +349,6 @@ def validate(val_loader, model, criterion, args):
     with torch.no_grad():
         end = time.time()
         for i, (images, target) in enumerate(val_loader):
-            # if not is_first:
-            #     break
             if args.gpu is not None:
                 images = images.cuda(args.gpu, non_blocking=True)
             if torch.cuda.is_available():
@@ -373,38 +372,39 @@ def validate(val_loader, model, criterion, args):
         #       .format(top1=top1, top5=top5))
 
     correct = pred_stack.eq(target_stack)
-    # print("correct shape:",correct.shape)
-    # pdb.set_trace()
+    
     res = []
     topk=(1, 5)
+    # topk=(5,)
     for k in topk:
-        # correct_k = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-
         # torch.bincount(target).cuda(args_.gpu, non_blocking=True) 
         if calculate_per_class_acc:
             for i in range(1000):
                 indices_of_occurance_of_i = target_stack[:k]==i
-                # if k==1:
-                #     indices_of_occurance_of_i = indices_of_occurance_of_i.squeeze()
-                # num_occurance_of_i = torch.sum(indices_of_occurance_of_i).cuda(args_.gpu, non_blocking=True)
-                num_occurance_of_i = torch.sum(indices_of_occurance_of_i)
+                # num_occurance_of_i = torch.sum(indices_of_occurance_of_i)
                 
                 correct_k = correct[:k]
                 if k==1:
+                    num_occurance_of_i = torch.sum(indices_of_occurance_of_i)
                     indices_of_occurance_of_i = indices_of_occurance_of_i.squeeze()
                     correct_k = correct_k.reshape(-1)
+                else:
+                    num_occurance_of_i = torch.sum(indices_of_occurance_of_i[0]) #counting the number of occurance using one row only
+                # pdb.set_trace()
                 correct_predicted_labels[i] += correct_k[indices_of_occurance_of_i].float().sum(0, keepdim=True).item()
                 total_labels[i] += num_occurance_of_i
             
             per_class_accuracy = correct_predicted_labels/total_labels
-            print('per_class_top'+str(k)+'_accuracy_epoch'+args.resume.split("_")[3]+":",per_class_accuracy)
+            # print('per_class_top'+str(k)+'_accuracy_epoch'+args.resume.split("_")[3]+":",per_class_accuracy)
+            # pdb.set_trace()
             torch.save(per_class_accuracy, 'models/run4/accuracy/per_class_top'+str(k)+'_accuracy_epoch'+args.resume.split("_")[3])
         
-        overall_correct_preds = correct[:k].reshape(-1).float().sum(0, keepdim=True)
-        overall_accuracy = overall_correct_preds/correct[:k].shape[1]
-        res.append(overall_accuracy.item())
-        # pdb.set_trace()
-    torch.save(res, 'models/run4/accuracy/overall/top'+str(topk[0])+'_top'+str(topk[1])+'_accuracy_epoch'+args.resume.split("_")[3])
+        if calculate_overall_acc:
+            overall_correct_preds = correct[:k].reshape(-1).float().sum(0, keepdim=True)
+            overall_accuracy = overall_correct_preds/correct[:k].shape[1]
+            res.append(overall_accuracy.item())
+    if calculate_overall_acc:
+        torch.save(res, 'models/run4/accuracy/overall/top'+str(topk[0])+'_top'+str(topk[1])+'_accuracy_epoch'+args.resume.split("_")[3])
 
 
 def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):