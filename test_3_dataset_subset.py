1c790a76b5379f9ccd5931c74668e8bb2ab39a87
Madhura <madhura.kbhat@gmail.com>
2021-06-30 10:52:45 +0530

    test_2_gc_resnet18.py: code to have a single model and just change the state dictionary for different epochs. Done by creating GCUtil object, calling create_output_folder function once, loading model state dictionary for each epoch and calling generate_grad_cam for each epoch; test_2_gc_resnet18.sh: passing folder containing saved models as argument to resume; test_3_dataset_subset.py: not using this file, tried but found using test_2_gc file easier;

---
+++ test_3_dataset_subset.py
@@ -0,0 +1,9 @@
+import os
+import sys
+sys.path.append('/srv/share3/mummettuguli3/code/')
+from utils.grad_cam_caller import GCUtil
+
+valdir = os.path.join("/coc/scratch/mummettuguli3/data/imagenet" , 'val')
+
+gc_util = GCUtil()
+gc_util.create_output_folder(output_dir='GRADCAM_MAPS/resnet18/', dataset="imagenet", class_list=["ostrich"], valdir=valdir, sample_count=2)
